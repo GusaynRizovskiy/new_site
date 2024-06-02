b0e5a86de8ad7be0057f8c23ae5477da403984bc
GusaynRizovskiy <makkks200426013@mail.ru>
2024-06-02 16:12:25 +0300

    Добавил модель Product, выполнил миграцию

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    discount = models.PositiveSmallIntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
