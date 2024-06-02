578fd751684e097c76b66d36464ca4ee2a4cda36
GusaynRizovskiy <makkks200426013@mail.ru>
2024-06-02 21:08:36 +0300

    Добавил модель Order, команды create_products.py,create_orders, update_orders.py. Добавлены новые шаблоны orders-list.html, products-list.html для отображения списка продуктов и заказов. Определена связь один к одному между пользователем и заказом и связь многие ко многим между продуктами и заказами. Добавлены две новые view функции OrdersList и ProductsList позволяющие передавать информацию о продуктах и заказах в шаблоны

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,10 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
 class Product(models.Model):
+    class Meta:
+        ordering = ["name", "price"]
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=300)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=8,decimal_places=2)
     discount = models.PositiveSmallIntegerField(default=0)
-    date_added = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+
+class Order(models.Model):
+    delivery_address = models.TextField(null=True,blank=True)
+    promocode = models.CharField(max_length=20,null=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.PROTECT)
+    products = models.ManyToManyField(Product, related_name="orders")
